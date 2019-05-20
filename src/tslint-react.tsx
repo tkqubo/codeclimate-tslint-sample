45b7a8b4e6d9de8b381121f9796ee015efb8a8f8
tkqubo <tk.qubo@gmail.com>
2019-05-20 14:56:09 +0900

    chore: add more react-tslint examples

--- src/tslint-react.tsx
+++ src/tslint-react.tsx
@@ -1,7 +1,18 @@
 'use strict';
 
-const jsxAlignment = <input
-  name='jsx-alignment'
+import * as React from 'react';
+
+// jsx-alignment
+export const jsxAlignment = <input name='jsx-alignment'
     id='jsx-alignment'
       value='jsx-alignment'
 />;
+
+// jsx-curly-spacing
+export const jsxCurlySpacing = <div id={ '42' }>Answer</div>;
+
+// jsx-wrap-multiline
+export const jsxWrapMultiline = <button type="submit">
+  Submit
+</button>;
+
